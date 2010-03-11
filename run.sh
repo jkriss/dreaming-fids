5514f25a5124ee422873120b231ba5b3b687bbaf
Jesse Kriss <jesse@jklabs.net>
2010-03-10 16:09:53 -0800

    adding controller for sinatra, creating run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env ruby
+
+`./fishcontrol server:start`
+`./fishcontrol open`
+sleep(10)
+`./fishcontrol run`
---
+++ sinatra_server.rb
@@ -0,0 +1,52 @@
+require 'fileutils'
+
+class SinatraServer
+  def pid_dir
+    "./"
+  end
+  def pid_path
+    pid_dir + "sinatra.pid"
+  end
+  def run
+    exec "shotgun -o 0.0.0.0 fishery.rb"
+  end
+  def start
+    if File.exist?(pid_path)
+      existing_pid = IO.read(pid_path).to_i
+      begin
+        Process.kill(0, existing_pid)
+        raise(Exception, "Server is already running with PID #{existing_pid}")
+      rescue Errno::ESRCH
+        STDERR.puts("Removing stale PID file at #{pid_path}")
+        FileUtils.rm(pid_path)
+      end
+    end
+    fork do
+      pid = fork do
+        Process.setsid
+        STDIN.reopen('/dev/null')
+        STDOUT.reopen('/dev/null', 'a')
+        STDERR.reopen(STDOUT)
+        run
+      end
+      FileUtils.mkdir_p(pid_dir)
+      File.open(pid_path, 'w') do |file|
+        file << pid
+      end
+    end
+  end
+  def stop
+    if File.exist?(pid_path)
+      pid = IO.read(pid_path).to_i
+      begin
+        Process.kill('TERM', pid)
+      rescue Errno::ESRCH
+        raise Exception, "Process with PID #{pid} is no longer running"
+      ensure
+        FileUtils.rm(pid_path)
+      end
+    else
+      raise Exception, "No PID file at #{pid_path}"
+    end
+  end
+end
